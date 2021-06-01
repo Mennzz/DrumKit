292636b3a31eb839df153bc4d9c945063f2dcaa5
Danfeng Mu <danfeng@geodelta.com>
2021-06-01 16:00:30 +0200

    add button animation

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ for (var i = 0; i < drumButtons.length; i++) {
   drumButtons[i].addEventListener('click', function() {
     var buttonInnerHTML = this.innerHTML;
     playSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
   })
 }
 // add keydown listener to the document
 document.addEventListener('keydown',function(event)
 {
   playSound(event.key);
+  buttonAnimation(event.key);
 })
 
 function playSound(letter)
@@ -20,32 +22,49 @@ function playSound(letter)
       var tom1 = new Audio('sounds/tom-1.mp3');
       tom1.play();
       break;
+
     case "a":
       var tom2 = new Audio('sounds/tom-2.mp3');
       tom2.play();
       break;
+
     case "s":
       var tom3 = new Audio('sounds/tom-3.mp3');
       tom3.play();
       break;
+
     case "d":
       var tom4 = new Audio('sounds/tom-4.mp3');
       tom4.play();
       break;
+
     case "j":
       var snare = new Audio('sounds/snare.mp3');
       snare.play();
       break;
+
     case "k":
       var crash = new Audio('sounds/crash.mp3');
       crash.play();
       break;
+
     case "l":
       var kick = new Audio('sounds/kick-bass.mp3');
       kick.play();
       break;
+
     default:
       console.log(buttonInnerHTML);
       break;
   }
+}
+
+function buttonAnimation(currentKey)
+{
+  var activeButton = document.querySelector("."+currentKey);
+  activeButton.classList.add("pressed");
+  // button animation
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  }, 100)
 }
